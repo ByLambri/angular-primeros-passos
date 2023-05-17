441462d3bc2b2518d0ffbae14fdce9b84608cd3e
Borja Sanchez Ruiz <5597180@alu.murciaeduca.es>
2023-05-17 10:25:26 +0200

    Fin de seccion 4

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title:string = 'Hola Mundo';
+  public counter:number = 19;
+
+  increseBy(value: number):void{
+    this.counter += value;
+  }
+
+  resetCounter(){
+    this.counter =19;
+  }
+}
